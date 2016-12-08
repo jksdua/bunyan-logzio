14843d1868a3a0c0b95b103126f7acc417e1d8fd
Or Yagel <or@soluto.com>
2016-12-08 11:51:44 +0200

    mutate log record to match logzio format

--- index.js
+++ index.js
@@ -13,18 +13,55 @@ function LogzioStream(logzioLogger, options) {
 LogzioStream.prototype = Object.create(stream.Writable.prototype);
 
 LogzioStream.prototype.write = function(rec) {
-
 	if (typeof rec !== 'object' && !Array.isArray(rec)) {
 		throw new Error('bunyan-logzio requires a raw stream. Please define the type as raw when setting up the bunyan stream.');
 	}
 
-	this.logzioLogger.log(rec);
-
+	this.logzioLogger.log(mutateToLogzioFormat(rec));
 };
 
 function Bunyan2Logzio (logzioLogger, options) {
 	this.type = 'raw';
 	this.stream = new LogzioStream(logzioLogger, options && options.stream);
+}
+
+function mutateToLogzioFormat(rec) {
+	if (rec.msg) {
+		rec = JSON.parse(JSON.stringify(rec));
+
+		rec.message = rec.msg;
+		rec.Level = mapLevelToName(rec.level);
+
+		delete rec.msg;
+		delete rec.level;
+	}
+
+	return rec;
+}
+
+// Levels
+var TRACE = 10;
+var DEBUG = 20;
+var INFO = 30;
+var WARN = 40;
+var ERROR = 50;
+var FATAL = 60;
+
+function mapLevelToName(level) {
+	switch (level) {
+		case TRACE:
+			return 'Trace';
+		case DEBUG:
+			return 'Debug';
+		case INFO:
+			return 'Info';
+		case WARN:
+			return 'Warn';
+		case ERROR:
+			return 'Error';
+		case FATAL:
+			return 'fatal';
+	}
 }
 
 module.exports.LogzioStream = LogzioStream;
